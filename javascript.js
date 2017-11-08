01c64df80704f62ba24e00f53e7e88a40a99f66b
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-11-08 15:36:13 +0100

    Correction

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 $('#shifumi').click(function(){
+  //On choisis unvaleur aléatoire lorsque l'on clique sur le bouton Shifumi !
   var random = Math.floor((Math.random() * 3) + 1);
+  //Suivant la valeur aléatoire obtenue, on modifis le contenu du paragraphe, ayant l'id #text, avec le contenu de la valeur aléatoire
   if(random == 1){
     $('#random').text('Pierres');
   }
@@ -11,6 +13,7 @@ $('#shifumi').click(function(){
   }
 });
 
+//Lorsque l'on clique sur l'un des éléments de la liste, il s'ajoute adns le paragraphe ayant pour id #player
 $('#pierres').click(function(){
   $('#player').text('Pierres');
 });
@@ -21,20 +24,28 @@ $('#ciseau').click(function(){
   $('#player').text('Ciseau');
 });
 
+//On déclare les variables correspondant aux victoires du PC et aux victoires du joueur
 var victoryPlayer = 0;
 var victoryPC = 0;
+//Lorsque l'on clique, on détermine qui, du joueur ou de l'ordinateur gagne
 $('#ok').click(function() {
+  //On définit des variales ayant pour valeur le contenu du paragraphe de l'id #random et de l'id #player
   var random = $('#random').text();
   var player = $('#player').text();
+  //On détermine ce qui se passe lorsque le joueur et l'ordinateur on la même réponse
   if(random == player){
     alert("Pas de victoire cette fois-ci !");
   }
+  //On détermine dans quels conditions le joueur gagne
   else if ((random == "Pierres" && player == "Feuille") || (random == "Feuille" && player == "Ciseau") || (random == "Ciseau" && player == "Pierres")) {
+  //Lorsque le joueur gagne, un mesage s'affiche et un compteur de victoire s'affiche
   alert('C\'est une victoire du joueur !');
   victoryPlayer++;
   $('#victoryPlayer').text('Voila le nombre de victoire du joueur -> ' + victoryPlayer);
   }
+
   else{
+    //Idem que pour le joueur
     alert('C\'est une victoire du PC !')
     victoryPC++;
     $('#victoryPC').text('Voila le nombre de victoire du PC -> ' + victoryPC);
